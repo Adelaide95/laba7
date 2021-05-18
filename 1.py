68ae59f78b030c3210e07f4f636cb62d150f3194
Adelaide95 <Adelaide2002@mail.ru>
2021-05-18 21:54:24 +0300

    add

---
+++ 1.py
@@ -0,0 +1,20 @@
+import sys
+a = tuple(map(float, input().split()))
+
+if len(a) != 10:
+    print("Неверный размер списка", file=sys.stderr)
+    exit(1)
+
+a_fil = tuple()
+
+for elem in a:
+    if elem > 8 and elem < 18 and elem % 10 == 0:
+        a_fil += tuple([elem])
+if len(a_fil) == 0:
+    print(0)
+else:
+    res = 1
+    for elem in a_fil:
+        res *= elem
+
+print(res)
---
+++ 2.py
@@ -0,0 +1,33 @@
+a = tuple(map(float, input().split()))
+
+C = float(input())
+count = 0
+for elem in a:
+    if elem > C:
+        count += 1
+
+print("Количество чисел больших С:", count)
+
+max_elem = 0
+for i, item in enumerate(a):
+    elem_abs = abs(item)
+    if elem_abs > max_elem:
+        max_elem, max_elem_ind = elem_abs, i
+
+if max_elem_ind == len(a) - 1:
+    print(0)
+else:
+    res = 1
+    for elem in a[max_elem_ind + 1:len(a)]:
+        res *= elem
+    print("Произведение чисел после максимального по модулю числа", res)
+
+a_pos = tuple()
+a_neg = tuple()
+
+for elem in a:
+    if elem >= 0:
+        a_pos += tuple([elem])
+    else:
+        a_neg += tuple([elem])
+print("Кортеж сначала с отрицательными, а потом позитивными числами:", a_neg + a_pos)
